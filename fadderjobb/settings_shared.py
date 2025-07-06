884c190aefb8f250cbda54762cbdbf3aedce416f
Andreas B <6439218+YouKnowBlom@users.noreply.github.com>
2025-07-06 19:06:09 +0200

    Set default auto field in settings and add related migrations

--- fadderjobb/settings_shared.py
+++ fadderjobb/settings_shared.py
@@ -40,6 +40,8 @@ ALLOWED_HOSTS = [
 
 AUTH_USER_MODEL = "accounts.User"
 
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
 # Impersonation
 # https://github.com/skorokithakis/django-loginas
 
