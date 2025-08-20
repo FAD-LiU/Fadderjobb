b3b02434d65a9f534cf1f76af06bf697ad95c1ea
Andreas B <6439218+YouKnowBlom@users.noreply.github.com>
2025-08-20 23:54:19 +0200

    Remove cronjob that spams activation emails hourly

--- accounts/cronjobs.py
+++ accounts/cronjobs.py
@@ -9,8 +9,3 @@ def update_points(num):
     for user in User.objects.filter().all():
         user.update_points()
     update_user_placings()
-
-@cron(5, -1, -1, -1, -1)
-def send_activation_emails(num):
-    for user in User.objects.filter(is_activated=False).all():
-        user.send_activation_email()
